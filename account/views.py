17244db236db92f1393928cb1c63751daf300bc0
BonusGned <jesus.moj@mail.com>
2020-10-22 15:39:03 +0300

    user editor

--- account/views.py
+++ account/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from django.contrib import messages
+
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            Profile.objects.create(user=new_user)
+            new_user.save()
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'account/register.html', {'user_form': user_form})
 
 
 @login_required
@@ -29,5 +47,31 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def load_profile(user):
+    try:
+        return user.profile
+    except:
+        profile = Profile.objects.create(user=user)
+        return profile
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=load_profile(request.user), data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Профиль успешно обновлён')
+        else:
+            messages.error(request, 'Ошибка при обновлении профиля')
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=load_profile(request.user))
+    return render(request, 'account/edit.html', {'user_form': user_form, 'profile_form': profile_form})
+
+
 
 
